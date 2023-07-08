176f78ed3cf8cc16eaa73672b8a520b04e262eca
himanshuabd17 <himanshu12061998@gmail.com>
2023-07-08 20:23:36 +0530

    using the prototype

--- object-prototypes.js
+++ object-prototypes.js
@@ -20,10 +20,20 @@ function Player(name,age,country){
     this.age=age;
     this.country=country;
 
-    this.details=function(){
-        console.log(this.name+" "+this.age);
-    }
+    // this.details=function(){
+    //     console.log(this.name+" "+this.age);
+    // }
 }
 const player1=new Player("abd",39,"south Africa");
 
+// const player2=new Player("Hashim Amla",41,"south Africa");
+
+// creating the function inside the prototype of Player constructor function
+
+Player.prototype.details=function(){
+    console.log(this.name+" "+this.age);
+}
+
 console.log(player1);
+
+// console.log(player2);
